1a2014bdfbf1fb4489afe92718e923ae58b16e98
viM <vishalmenon.92@gmail.com>
2019-04-01 16:29:32 +0530

    v0.8.0

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -78362,7 +78362,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38309" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
